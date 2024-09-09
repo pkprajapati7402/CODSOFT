e0baec9c81a89ac95ed01bfde38ab200a458abfd
Prince <167101466+pkprajapati7402@users.noreply.github.com>
2024-09-09 07:44:27 -0700

    Update and rename 01-Sparse-matrix.cpp to Task1_Number_guessing_game.cpp

---
+++ C++ Task/Task1_Number_guessing_game.cpp
@@ -0,0 +1,12 @@
+/*
+Number Guessing Game
+
+Create a program that generates a random number and asks the user to guess it.
+Provide feedback on whetehr the guess is too high or too low untill the user guesses the corret number.
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+   
+}
